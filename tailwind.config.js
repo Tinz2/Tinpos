31b453bf86794d6efa5a62b8142490d52ac07010
Tinz2 <tinapat2547@gmail.com>
2024-11-19 21:27:33 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans:['Kanit', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
